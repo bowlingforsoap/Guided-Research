424322ac5faba8004d08a8a9fbc7dd7855af214b
Strelchenko Vadym <bowlingforsoap@gmail.com>
2016-11-08 21:50:00 +0100

    generateScalarField and load it into texture

--- GuidedResearch/geometry.glsl
+++ GuidedResearch/geometry.glsl
@@ -3,12 +3,14 @@
 layout (points) in;
 layout (line_strip, max_vertices = 2) out;
 
+uniform sampler2D scalarField;
+
 void main() {
   gl_Position = gl_in[0].gl_Position;
   EmitVertex();
 
   gl_Position = gl_in[0].gl_Position + vec4(.3f, .3f, 0.f, 0.f);
   EmitVertex();
-  
+
   EndPrimitive();
 }
--- GuidedResearch/main.cpp
+++ GuidedResearch/main.cpp
@@ -1,5 +1,20 @@
 #include <util/GLFWinitializer.h>
 #include <util/Shader.h>
+#include <SOIL.h>
+
+GLfloat* generateScalarField(GLint width, GLint height, GLfloat minX, GLfloat minY, GLfloat maxX, GLfloat maxY) {
+	GLfloat* scalarFiled = new float[width * height];
+	float xDelta = abs(maxX - minX) / width;
+	float yDelta = abs(maxY - minY) / height;
+
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			scalarFiled[i * height + j] = sin(minX + xDelta * i) * cos(minY + yDelta * j);
+		}
+	}
+
+	return scalarFiled;
+}
 
 int main() {
 	GLint width = 1600, height = 1000;
@@ -9,6 +24,21 @@ int main() {
 
 	// Setup shaders
 	Shader shader("vert.glsl", "frag.glsl", "geometry.glsl");
+
+	// Scalar Field setup
+	GLint fieldWidth = 600, fieldHeight = 600;
+	GLfloat* scalarField = generateScalarField(fieldWidth, fieldHeight, -3, -2, 3, 2);
+	// Store scalar field into a texture
+	GLuint scalarFieldTex;
+	glGenTextures(1, &scalarFieldTex);
+	glBindTexture(GL_TEXTURE_2D, scalarFieldTex);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, fieldWidth, fieldHeight, 0, GL_RED, GL_FLOAT, scalarField);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	cout << glGetError() << endl;
+	delete[] scalarField;
+	// Set texture uniform
+	shader.Use();
+	glUniform1i(glGetUniformLocation(shader.Program, "scalarField"), 0);
 
 	// Points to render
 	GLfloat points[] {
@@ -38,6 +68,7 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		shader.Use();
+		glBindTexture(GL_TEXTURE_2D, scalarFieldTex);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_POINTS, 0, 1);
 		glBindVertexArray(0);
